9d41e725e9539fa81b098cb26a5f6ad03097068b
VVJS-22 <s.jayeshchennai@gmail.com>
2021-05-31 15:03:41 +0530

    initial

---
+++ solution/script.js
@@ -0,0 +1,105 @@
+
+// if else statement
+
+// const number = parseInt(prompt("Enter a n
+
+// Math.random()*(max-min+1)+min;
+
+const number = Math.floor(Math.random()*10);
+console.log(number);
+
+if (number % 2 == 0) {
+
+
+    console.log("Even");
+
+
+} else {
+
+
+    console.log("Odd");
+
+
+}
+
+if (number % 2 == 0 && number != 0) {
+
+
+    console.log("Even");
+
+
+} else if (number == 0) {
+
+
+    console.log("Zero")
+
+
+} else {
+
+
+    console.log("Odd");
+
+
+}
+
+// for loop
+
+
+
+
+for (let i = number; i < 10; i++) {
+
+    console.log(i);
+
+
+}
+
+let i = number;
+
+while (i < 10) {
+
+
+    console.log(i);
+
+    i++;
+
+
+}
+
+// do {
+
+//     console.log(i);
+
+//     i++;
+
+
+// } while (i < 10);
+
+
+let k = number;
+
+switch (k) {
+
+    case 5:
+        console.log("Buzz");
+        break;
+    case 0:
+        console.log ("Zero");
+        break;
+    default:
+        console.log("Fizz");
+}
+
+for (let i = 0; i < 10; i++) {
+
+    
+
+    if (i == 5) {
+
+        continue;
+
+    }
+    console.log (i);
+    
+}
+
